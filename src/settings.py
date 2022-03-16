a2bc684b39852782595a64f5a9cc6f565fa83825
alexander febres <alexanderfebresth@gmail.com>
2022-03-16 08:23:23 -0500

    Getting ready to deploy

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,7 @@
 
+import dj_database_url
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,12 +84,14 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
 
 DATETIME_FORMAT = "%Y-%m-%d%H:%M:%S"
 
@@ -131,3 +135,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
